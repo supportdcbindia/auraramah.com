3441b7db01db4e3bfb44376df9bf9854e90acf17
supportdcbindia <support@dcbindia.in>
2026-09-22 15:56:28 +0530

    Update inquiry-action.php

--- inquiry-action.php
+++ inquiry-action.php
@@ -4,9 +4,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// echo "<pre>";print_r($_POST);
-// echo "<pre>";print_r($_REQUEST);
-// exit;
+echo "<pre>";print_r($_POST);
+echo "<pre>";print_r($_REQUEST);
+exit;
 
 $myfile = fopen("logs.txt", "a+") or die("Unable to open file!");
 
